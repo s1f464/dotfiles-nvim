3d307ada5549f2fdca0ce7068810584a0747f3cd
s1f464 <>
2023-11-07 07:19:32 +0000

    Restore cursor style after exiting

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,12 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
+  callback = function()
+    vim.opt.guicursor = "a:hor10-blinkwait500-blinkon500-blinkoff500"
+  end,
+})
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = {
     "css",
