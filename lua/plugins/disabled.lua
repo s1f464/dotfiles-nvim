b61063eb2b451b7c57767174a3dd556e0472295e
s1f464 <>
2023-12-24 10:12:06 +0000

    Disable some plugins
    Disabled plugins:
    
    - mason
    - mason-lspconfig
    - neoconf

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,7 +1,10 @@
 return {
   { "catppuccin/nvim", name = "catppuccin", enabled = false },
+  { "folke/neoconf.nvim", enabled = false },
   { "folke/todo-comments.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
   { "nvim-tree/nvim-web-devicons", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
+  { "williamboman/mason-lspconfig.nvim", enabled = false },
+  { "williamboman/mason.nvim", enabled = false },
 }
