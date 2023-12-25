b267b17c3a6d43daf5031b52c221aa01ee7abdc7
s1f464 <>
2023-12-25 10:35:41 +0000

    Remove unused plugin specs

--- lua/plugins/dashboard.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "nvimdev/dashboard-nvim",
-  opts = function(_, opts)
-    local version = vim.split(vim.fn.execute("version"), "\n")[3]:sub(6)
-    local logo = string.rep("\n", 8) .. string.format("NVIM %s", version) .. "\n\n"
-    opts.config.header = vim.split(logo, "\n")
-  end,
-}
