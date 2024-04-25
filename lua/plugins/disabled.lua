5d6daf10d4881ac247e34db7faf83f6b0fac2456
s1f464 <>
2024-04-25 15:07:06 +0000

    lspconfig: add mason and mason-lspconfig

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -5,6 +5,4 @@ return {
   { "folke/tokyonight.nvim", enabled = false },
   { "nvim-tree/nvim-web-devicons", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
-  { "williamboman/mason-lspconfig.nvim", enabled = false },
-  { "williamboman/mason.nvim", enabled = false },
 }
