97b487dcf70602a78a588a3bc995b82e649a4cfb
s1f464 <>
2024-05-24 15:42:05 +0000

    feat(plugin): add everforest, remove monokai-pro

--- lua/lazyspecs/colorschemes.lua
+++ lua/lazyspecs/colorschemes.lua
@@ -1,10 +1,22 @@
 return {
-  -- Monokai Pro theme for Neovim
-  "loctvl842/monokai-pro.nvim",
+  -- A Lua port of the Everforest colour scheme.
+  "neanias/everforest-nvim",
+  lazy = false,
   priority = 1000,
-  config = function()
+  opts = {
+    background = "hard",
+    colours_override = function(palette)
+      palette.bg_dim = "#000000"
+      palette.bg0 = "#000000"
+      palette.bg1 = "#0a0e0c"
+      palette.bg2 = "#131715"
+    end,
+  },
+  config = function(_, opts)
+    require("everforest").setup(opts)
+
     if vim.env.COLORTERM == "truecolor" then
-      vim.cmd([[colorscheme monokai-pro]])
+      vim.cmd([[colorscheme everforest]])
     end
   end,
 }
