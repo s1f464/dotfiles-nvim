65958d5264b479759104ed6bf882e7e21065511f
s1f464 <>
2024-05-24 17:35:38 +0000

    fix(plugin): fix typos in bufferline opts

--- lua/lazyspecs/bufferline.lua
+++ lua/lazyspecs/bufferline.lua
@@ -8,7 +8,7 @@ return {
       buffer_close_icon = "x",
       close_icon = "x",
       diagnostics = "nvim_lsp",
-      show_biffer_icons = false,
+      show_buffer_icons = false,
       show_buffer_close_icons = false,
       always_show_bufferline = false,
     },
