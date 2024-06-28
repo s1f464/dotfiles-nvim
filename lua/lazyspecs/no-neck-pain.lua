3cf81f8d46dbf544aeda89b97c40f16efb1c0a7d
s1f464 <>
2024-06-28 10:05:41 +0000

    docs(no-neck-pain): add description

--- lua/lazyspecs/no-neck-pain.lua
+++ lua/lazyspecs/no-neck-pain.lua
@@ -1,4 +1,6 @@
 return {
+  -- Dead simple plugin to center the currently focused buffer to the middle of
+  -- the screen.
   "shortcuts/no-neck-pain.nvim",
   version = "*",
   cmd = "NoNeckPain",
