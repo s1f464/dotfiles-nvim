9c912b64dda168cb326b43f780d0713086471d42
s1f464 <>
2024-07-01 00:07:54 +0000

    feat(plugin): add nvim-treesitter-textobjects

--- lua/lazyspecs/nvim-treesitter.lua
+++ lua/lazyspecs/nvim-treesitter.lua
@@ -1,13 +1,66 @@
 return {
   -- Nvim Treesitter configurations and abstraction layer
   "nvim-treesitter/nvim-treesitter",
-  event = { "VeryLazy" },
+  dependencies = {
+    -- Syntax aware text-objects, select, move, swap, and peek support.
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  event = "VeryLazy",
   lazy = vim.fn.argc(-1) == 0,
   build = ":TSUpdate",
   cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
   opts = {
     highlight = { enable = true },
     indent = { enable = true },
+    textobjects = {
+      move = {
+        enable = true,
+        goto_next_start = {
+          ["]f"] = "@function.outer",
+          ["]c"] = "@class.outer",
+          ["]a"] = "@parameter.inner",
+        },
+        goto_next_end = {
+          ["]F"] = "@function.outer",
+          ["]C"] = "@class.outer",
+          ["]A"] = "@parameter.inner",
+        },
+        goto_previous_start = {
+          ["[f"] = "@function.outer",
+          ["[c"] = "@class.outer",
+          ["[a"] = "@parameter.inner",
+        },
+        goto_previous_end = {
+          ["[F"] = "@function.outer",
+          ["[C"] = "@class.outer",
+          ["[A"] = "@parameter.inner",
+        },
+      },
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = {
+            query = "@class.inner",
+            desc = "Select inner part of a class region",
+          },
+          ["as"] = {
+            query = "@scope",
+            query_group = "locals",
+            desc = "Select language scope",
+          },
+        },
+        selection_modes = {
+          ["@parameter.outer"] = "v",
+          ["@function.outer"] = "V",
+          ["@class.outer"] = "<c-v>",
+        },
+        include_surrounding_whitespace = true,
+      },
+    },
   },
   config = function(_, opts)
     require("nvim-treesitter.configs").setup(opts)
