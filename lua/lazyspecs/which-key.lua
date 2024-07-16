24889559a12381ae095bf9fbb2455b4d3d629464
s1f464 <>
2024-07-16 02:09:16 +0000

    fix(which-key): remove deprecated opts

--- lua/lazyspecs/which-key.lua
+++ lua/lazyspecs/which-key.lua
@@ -1,14 +1,9 @@
 return {
-  -- WhichKey is a lua plugin for Neovim 0.5 that displays a popup with possible
-  -- key bindings of the command you started typing.
+  -- WhichKey helps you remember your Neovim keymaps, by showing available
+  -- keybindings in a popup as you type.
   "folke/which-key.nvim",
   event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
   opts = {
-    window = { margin = { 1, 0, 0, 0.6 } },
-    layout = { height = { min = 4, max = 75 } },
+    preset = "helix",
   },
 }
