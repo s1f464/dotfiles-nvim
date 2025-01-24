a33d842d5cce7f816b816413df62c17c120146e4
s1f464 <>
2025-01-24 03:15:00 +0000

    feat(blink.cmp): auto show documentation

--- lua/lazyspecs/blink.lua
+++ lua/lazyspecs/blink.lua
@@ -6,6 +6,12 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
+    completion = {
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 500,
+      },
+    },
     sources = {
       default = { "lazydev", "lsp", "path", "snippets", "buffer" },
       cmdline = {},
