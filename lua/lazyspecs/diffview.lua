27ec200fce09b75672eadbb186d6cf277707139e
s1f464 <>
2025-04-30 19:57:03 +0000

    feat(plugin): add diffview

---
+++ lua/lazyspecs/diffview.lua
@@ -0,0 +1,9 @@
+return {
+  -- Single tabpage interface for easily cycling through diffs for all modified
+  -- files for any git rev.
+  "sindrets/diffview.nvim",
+  cmd = "DiffviewOpen",
+  opts = {
+    use_icons = false,
+  },
+}
