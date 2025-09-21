6756cbfe5a3eb6058a1bec6b09d181b8ba7f0256
s1f464 <>
2025-09-21 08:54:13 +0000

    refactor(lspconfig): use vim.lsp

--- lua/lazyspecs/lspconfig.lua
+++ lua/lazyspecs/lspconfig.lua
@@ -1,12 +1,7 @@
 return {
   -- Quickstart configs for Nvim LSP
-  -- :help lspconfig-server-configurations
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    -- Performant, batteries-included completion plugin for Neovim
-    "saghen/blink.cmp",
-  },
   opts = {
     servers = {
       astro = {},
@@ -49,12 +44,36 @@ return {
       yamlls = {},
     },
   },
-  config = function(_, opts)
-    local lspconfig = require("lspconfig")
-
-    for s, o in pairs(opts.servers) do
-      o.capabilities = require("blink.cmp").get_lsp_capabilities(o.capabilities)
-      lspconfig[s].setup(o)
-    end
+  config = function()
+    vim.lsp.enable("astro")
+    vim.lsp.enable("basedpyright")
+    vim.lsp.enable("bashls")
+    vim.lsp.enable("biome")
+    vim.lsp.enable("cssls")
+    vim.lsp.enable("gopls")
+    vim.lsp.enable("html")
+    vim.lsp.enable("jsonls")
+    vim.lsp.enable("lua_ls")
+    vim.lsp.enable("prismals")
+    vim.lsp.enable("ruff")
+    vim.lsp.enable("rust_analyzer")
+    vim.lsp.enable("svelte")
+    vim.lsp.enable("tailwindcss")
+    vim.lsp.enable("taplo")
+    vim.lsp.config("vtsls", {
+      settings = {
+        typescript = {
+          inlayHints = {
+            parameterNames = { enabled = "literals" },
+            parameterTypes = { enabled = true },
+            variableTypes = { enabled = true },
+            propertyDeclarationTypes = { enabled = true },
+            functionLikeReturnTypes = { enabled = true },
+            enumMemberValues = { enabled = true },
+          },
+        },
+      },
+    })
+    vim.lsp.enable("yamlls")
   end,
 }
